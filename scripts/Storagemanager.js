cb69b1f8a4747e54bb5291aec73b64e215fc4bec
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-27 02:31:57 +0330

    feat: complete storage manager
    add functions to check has key or clear storage key set and get

--- scripts/Storagemanager.js
+++ scripts/Storagemanager.js
@@ -1,3 +1,51 @@
+/**
+ * @param  {string} key github username
+ * @param  {JSON} value github user data
+ * @returns {void}
+ * @description set the user data to local storage
+ * @example setLocalStorage('username', {name: 'John Doe', email: ''})
+ * 
+ */
 const setLocalStorage = (key, value) => {
     localStorage.setItem(key, JSON.stringify(value));
 };
+/**
+ * @param  {string} key github username
+ * @returns {JSON} user data
+ * @description get the user data from local storage
+ * @example getLocalStorage('username')
+ */
+const getLocalStorage = (key) => {
+    return JSON.parse(localStorage.getItem(key));
+}
+/**
+ * @param  {string} key github username
+ * @returns {void}
+ * @description remove the user data from local storage
+ * @example removeLocalStorage('username')
+ * 
+ */
+const removeLocalStorage = (key) => {
+    localStorage.removeItem(key);
+}
+/**
+ * @returns {void}
+ * @description clear all the data from local storage
+ * @example clearLocalStorage()
+ * 
+ */
+const clearLocalStorage = () => {
+    localStorage.clear();
+}
+/**
+ * @param  {string} key github username
+ * @returns {boolean} true if the key exists
+ * @description check if the key exists in local storage
+ * @example hasKey('username')
+ * 
+ */
+const hasKey = (key) => {
+    return localStorage.hasOwnProperty(key);
+}
+
+export {setLocalStorage, getLocalStorage, removeLocalStorage, clearLocalStorage, hasKey};
