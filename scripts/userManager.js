d5f90b0ef36defb3d6f024b479df99b1bf65edf2
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-27 03:34:27 +0330

    docs: user Manager files

--- scripts/userManager.js
+++ scripts/userManager.js
@@ -6,7 +6,15 @@ import {
   hasKey,
 } from "./storageManager.js";
 import { getUserData, getUserRepos, mostUsedLanguages } from "./fetchData.js";
-
+/**
+ * @param  {string} username
+ * @returns {JSON} user data
+ * @description get the user data from github api or local storage if it exists there
+ * also set the user data to local storage
+ * concat the user data with the user repos and the most used languages
+ * @example getData('username')
+ * 
+ */
 const getData = (username) => {
     // check if the user data exists in local storage
     if (hasKey(username)) {
@@ -26,7 +34,12 @@ const getData = (username) => {
     // return the user data
     return { data, repos, mostUsed };
 };
-
+/**
+ * @returns {void}
+ * @description clear all the data from local storage
+ * @example clearData()
+ * use this function in the start of the app
+ */
 const clearData = () => {
     // clear all the data from local storage
     clearLocalStorage();
