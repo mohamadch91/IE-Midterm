03788d0c438dd96494648542c1a8c8b81e547990
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-27 14:58:54 +0330

    fix: refresh on form submit app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,8 @@
 import { getData, clearData } from "./userManager";
 
 
-const onSubmit = () => {
+const onSubmit = (e) => {
+    e.preventDefault();
     const username= document.getElementById('username').value;
     const user = getData(username);
     
