1f9f4f38f38713ad4783d85e572a17b24ab4f723
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-27 03:42:58 +0330

    feat:form  onsubmit function create

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,9 @@
 import { getData, clearData } from "./userManager";
 
+
+const onSubmit = () => {
+    const username= document.getElementById('username').value;
+    const user = getData(username);
+    
+    // do something with user
+}
