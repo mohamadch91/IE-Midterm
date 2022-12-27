96b51b426b417c022485c8fdfa249fab0ccdf63c
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-28 02:27:14 +0330

    fix: loader style

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,14 @@ import { getData, clearData } from "./userManager.js";
 
 const onSubmit = async (e) => {
     e.preventDefault();
-    const loader= document.('loader');
+    const loader= document.getElementById('loader');
+    loader.style.display='block';
+    const info= document.getElementById('info');
+    info.style.display='none';
     const username= document.getElementById('username').value;
     const user =  await getData(username);
+    loader.style.display='none';
+    info.style.display='block';
     return false;
     // do something with user
 }
