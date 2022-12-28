a62762c16fabe7eb48047e23984a837c0d9ae5ff
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-28 04:12:02 +0330

    docs: app.js documantation

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,10 @@
 import { getData, clearData } from "./userManager.js";
+/**
+ * @constant {object} icon_dic
+ * @description dictionary of icons for the most used languages
+ * @example icon_dic['C++'] = 'c-plusplus'
+ * 
+ */
 const icon_dic = {
   "C++": "c-plusplus",
   "C#": "csharp",
@@ -24,7 +30,14 @@ const icon_dic = {
   Perl: "perl",
   Lua: "lua",
 };
-
+/**
+ * @param  {string} msg message to be displayed
+ * @param  {string} color color of the alert
+ * @returns {void}
+ * @description display the alert with the message and the color
+ * @example styledAlert('Data is from local storage', 'green')
+ * 
+ */
 export const styledAlert = (msg, color) => {
   document.getElementById("alert").style.display = "block";
   document.getElementById("alert").style.backgroundColor = color;
@@ -36,16 +49,47 @@ export const styledAlert = (msg, color) => {
 
 
 };
+/**
+ * @param  {string} id id of the element
+ * @param  {string} content content to be added
+ * @returns {void}
+ * @description add the content to the element with the id
+ * @example changeContent('most', 'C++')
+ * 
+ */
 const changeContent = (id, content) => {
   document.getElementById(id).append(" " + content);
 };
+/**
+ * @param  {string} id id of the element
+ * @param  {string} content reset the content of the element with the id
+ * @returns {void}
+ * @description reset the content of the element with the id
+ * @example resetContent('most', 'Most used language: ')
+ * 
+ */
 const resetContent = (id,content) => {
     document.getElementById(id).innerHTML = content;
 };
+/**
+ * @param  {string} id id of the element
+ * @param  {string} src src of the image URL
+ * @returns {void}
+ * @description change the src of the image with the id
+ * @example changeSrc('most_img', 'https://skillicons.dev/icons?i=c-plusplus')
+ * 
+ */
 const changeSrc = (id, src) => {
   document.getElementById(id).setAttribute("src", src);
 };
-
+/**
+ * @param  {object} user user data
+ * @param  {string} most_used_lang most used language
+ * @returns {void}
+ * @description change the page content with the user data and the most used language
+ * @example changePage(user, 'C++')
+ * 
+ */
 const changePage = (user, most_used_lang) => {
   changeContent("most", most_used_lang);
   changeSrc(
@@ -62,6 +106,12 @@ const changePage = (user, most_used_lang) => {
   changeContent("company", user.company);
   changeContent("blog", user.blog);
 };
+/**
+ * @returns {void}
+ * @description reset the page content
+ * @example resetPage()
+ * 
+ */
 const resetPage = () => {
     resetContent("most", "Most used language: ");
     changeSrc("most_img", "");
@@ -75,22 +125,36 @@ const resetPage = () => {
     resetContent("company", "Company: ");
     resetContent("blog", "Blog: ");
 };
+/**
+ * @param  {Event} e submit event
+ * @returns {void}
+ * @description submit event handler for user search
+ * @example onSubmit(e)
+ * 
+ */
 const onSubmit = async (e) => {
+    // prevent the default action of the submit event (refreshing the page)
   e.preventDefault();
+    // reset the page
   resetPage();
+    // check if the user is online
   if (window.navigator.onLine === false) {
     styledAlert("No internet connection", "red");
     return false;
     }
+    // show the loader 
   const loader = document.getElementById("loader");
   loader.style.display = "block";
+    // hide the info    
   const info = document.getElementById("info");
   info.style.display = "none";
+    // get the username from the input
   const username = document.getElementById("username").value;
+    // get the user data from the API
   const user = await getData(username);
   const most_used_lang = user.most_used_lang;
   const user_data = user.u_data;
-  console.log(user)
+    // check if the user data is null or undefined or empty string or API rate limit exceeded or user not found
   if (user_data === undefined) {
     styledAlert("Network error", "red");
   } else if (user_data.message == "API rate limit exceeded for") {
@@ -104,13 +168,14 @@ const onSubmit = async (e) => {
         user_data[key] = "Not available";
       }
     }
-         
+    // change the page content
     changePage(user_data, most_used_lang);
   }
+    // hide the loader
   loader.style.display = "none";
+    // show the info
   info.style.display = "block";
   return false;
-  // do something with user
 };
 
 // window.onload =clearData;
