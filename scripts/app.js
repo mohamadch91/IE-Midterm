de66d329ad0bb942b1f920f9ed888d8d7f836080
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-28 04:15:48 +0330

    feat: icon dic update

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,7 @@ import { getData, clearData } from "./userManager.js";
  * @constant {object} icon_dic
  * @description dictionary of icons for the most used languages
  * @example icon_dic['C++'] = 'c-plusplus'
- * 
+ *
  */
 const icon_dic = {
   "C++": "c-plusplus",
@@ -29,6 +29,17 @@ const icon_dic = {
   Scala: "scala",
   Perl: "perl",
   Lua: "lua",
+  "Objective-C": "objectivec",
+  "Jupyter Notebook": "jupyter",
+  CMake: "cmake",
+  CoffeeScript: "coffeescript",
+  Shell: "powershell",
+  TeX: "tex",
+  PowerShell: "powershell",
+  Elixir: "elixir",
+  Node: "nodejs",
+    "Visual Basic": "visualbasic",
+    Nodejs: "nodejs",
 };
 /**
  * @param  {string} msg message to be displayed
@@ -36,18 +47,16 @@ const icon_dic = {
  * @returns {void}
  * @description display the alert with the message and the color
  * @example styledAlert('Data is from local storage', 'green')
- * 
+ *
  */
 export const styledAlert = (msg, color) => {
   document.getElementById("alert").style.display = "block";
   document.getElementById("alert").style.backgroundColor = color;
 
   document.getElementById("alert-text").innerHTML = msg;
-    setTimeout(() => {
+  setTimeout(() => {
     document.getElementById("alert").style.display = "none";
-    }, 3000);
-
-
+  }, 3000);
 };
 /**
  * @param  {string} id id of the element
@@ -55,7 +64,7 @@ export const styledAlert = (msg, color) => {
  * @returns {void}
  * @description add the content to the element with the id
  * @example changeContent('most', 'C++')
- * 
+ *
  */
 const changeContent = (id, content) => {
   document.getElementById(id).append(" " + content);
@@ -66,10 +75,10 @@ const changeContent = (id, content) => {
  * @returns {void}
  * @description reset the content of the element with the id
  * @example resetContent('most', 'Most used language: ')
- * 
+ *
  */
-const resetContent = (id,content) => {
-    document.getElementById(id).innerHTML = content;
+const resetContent = (id, content) => {
+  document.getElementById(id).innerHTML = content;
 };
 /**
  * @param  {string} id id of the element
@@ -77,7 +86,7 @@ const resetContent = (id,content) => {
  * @returns {void}
  * @description change the src of the image with the id
  * @example changeSrc('most_img', 'https://skillicons.dev/icons?i=c-plusplus')
- * 
+ *
  */
 const changeSrc = (id, src) => {
   document.getElementById(id).setAttribute("src", src);
@@ -88,7 +97,7 @@ const changeSrc = (id, src) => {
  * @returns {void}
  * @description change the page content with the user data and the most used language
  * @example changePage(user, 'C++')
- * 
+ *
  */
 const changePage = (user, most_used_lang) => {
   changeContent("most", most_used_lang);
@@ -110,51 +119,51 @@ const changePage = (user, most_used_lang) => {
  * @returns {void}
  * @description reset the page content
  * @example resetPage()
- * 
+ *
  */
 const resetPage = () => {
-    resetContent("most", "Most used language: ");
-    changeSrc("most_img", "");
-    changeSrc("avatar", "./images/profile.png");
-    resetContent("fullname", "Full name: ");
-    resetContent("uname", "Username: ");
-    resetContent("bio", "Bio: ");
-    resetContent("followers", "Followers: ");
-    resetContent("following", "Following: ");
-    resetContent("location", "Location: ");
-    resetContent("company", "Company: ");
-    resetContent("blog", "Blog: ");
+  resetContent("most", "Most used language: ");
+  changeSrc("most_img", "");
+  changeSrc("avatar", "./images/profile.png");
+  resetContent("fullname", "Full name: ");
+  resetContent("uname", "Username: ");
+  resetContent("bio", "Bio: ");
+  resetContent("followers", "Followers: ");
+  resetContent("following", "Following: ");
+  resetContent("location", "Location: ");
+  resetContent("company", "Company: ");
+  resetContent("blog", "Blog: ");
 };
 /**
  * @param  {Event} e submit event
  * @returns {void}
  * @description submit event handler for user search
  * @example onSubmit(e)
- * 
+ *
  */
 const onSubmit = async (e) => {
-    // prevent the default action of the submit event (refreshing the page)
+  // prevent the default action of the submit event (refreshing the page)
   e.preventDefault();
-    // reset the page
+  // reset the page
   resetPage();
-    // check if the user is online
+  // check if the user is online
   if (window.navigator.onLine === false) {
     styledAlert("No internet connection", "red");
     return false;
-    }
-    // show the loader 
+  }
+  // show the loader
   const loader = document.getElementById("loader");
   loader.style.display = "block";
-    // hide the info    
+  // hide the info
   const info = document.getElementById("info");
   info.style.display = "none";
-    // get the username from the input
+  // get the username from the input
   const username = document.getElementById("username").value;
-    // get the user data from the API
+  // get the user data from the API
   const user = await getData(username);
   const most_used_lang = user.most_used_lang;
   const user_data = user.u_data;
-    // check if the user data is null or undefined or empty string or API rate limit exceeded or user not found
+  // check if the user data is null or undefined or empty string or API rate limit exceeded or user not found
   if (user_data === undefined) {
     styledAlert("Network error", "red");
   } else if (user_data.message == "API rate limit exceeded for") {
@@ -164,16 +173,20 @@ const onSubmit = async (e) => {
   } else {
     // check if null or undefined or empty string
     for (const key in user_data) {
-      if (user_data[key] == null || user_data[key] == undefined || user_data[key] == "") {
+      if (
+        user_data[key] == null ||
+        user_data[key] == undefined ||
+        user_data[key] == ""
+      ) {
         user_data[key] = "Not available";
       }
     }
     // change the page content
     changePage(user_data, most_used_lang);
   }
-    // hide the loader
+  // hide the loader
   loader.style.display = "none";
-    // show the info
+  // show the info
   info.style.display = "block";
   return false;
 };
